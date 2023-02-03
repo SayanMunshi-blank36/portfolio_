75d1dae8cc3857fa54325eca69e8e951e53b47b9
Sayan <sayanmunshi36@gmail.com>
2023-02-04 00:33:32 +0530

    Hero Section made

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,37 @@
+import "./assets/css/Hero.css";
+import {
+  FiGithub,
+  FiTwitter,
+  FiLinkedin,
+  FiInstagram,
+  FiMail,
+} from "react-icons/fi";
+
+const Hero = () => {
+  return (
+    <section className="hero_section">
+      <h1 className="hero_header">Sayan Munshi.</h1>
+      <h2 className="hero_tag">
+        A Passionate FullStack Developer with a knack to learn new Technologies.
+      </h2>
+      <p className="hero_desc">
+        I am a design minded <span>FullStack Developer </span>
+        based on Kolkata, India. I like building solid and scalable frontend
+        products with great user experience. I like to write clean and elegant
+        code.
+      </p>
+      <p className="hero_desc">
+        Connect with me if you are interested to collaborate with me. ✌
+      </p>
+      <div className="socials">
+        <FiGithub className="social_icons" />
+        <FiTwitter className="social_icons" />
+        <FiLinkedin className="social_icons" />
+        <FiInstagram className="social_icons" />
+        <FiMail className="social_icons" />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
