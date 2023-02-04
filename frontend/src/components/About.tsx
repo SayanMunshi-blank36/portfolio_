279f30e6e61cf9f9841d68628139e5a77d18e0e9
Sayan <sayanmunshi36@gmail.com>
2023-02-04 17:10:43 +0530

    About Section Improvement

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -11,7 +11,16 @@ import {
   SiJest,
   SiWeb3Dotjs,
   SiSolidity,
+  SiFigma,
+  SiHashnode,
 } from "react-icons/si";
+import {
+  FiGithub,
+  FiTwitter,
+  FiLinkedin,
+  FiInstagram,
+  FiMail,
+} from "react-icons/fi";
 
 const About = () => {
   return (
@@ -20,24 +29,80 @@ const About = () => {
       <div className="abt_content">
         <div className="abt_write">
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur,
-            provident unde? Suscipit sed saepe, autem exercitationem excepturi
-            quas accusamus perspiciatis quos officiis labore dolor aliquam animi
-            fugiat ipsa quisquam laborum sapiente unde, necessitatibus aliquid
-            ex. Nesciunt odit aperiam nobis? Amet aspernatur ex repudiandae
-            facere omnis odit natus esse harum. Illum.
+            Hello! My name is Sayan Munshi and I like making things for the web.
+            Currently I am pursuing my B.Tech degree on Information Technology
+            from the Institute of Enginnering and Management, Kolkata. My
+            interest in Web Development started back in 2021. At the very
+            beginning I got my hands on <span>HTML/CSS and JAVASCRIPT </span>
+            and from there my journey began.
           </p>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde id
-            corporis, qui excepturi totam animi ea nemo dolores dignissimos ex.
-            Soluta quasi sint asperiores iusto labore cum sequi dolores
-            exercitationem?
+            Fast forwarding to present day I have learnt a handful of frontend
+            technologies such as{" "}
+            <span>ReactJS, NextJS, TailwindCSS, Redux, Redux Toolkit, </span>
+            etc. along with a few backend technologies such as{" "}
+            <span> NodeJS, ExpressJS </span> and <span> MongoDB</span>.
           </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime
-            eos aperiam quibusdam, consequuntur eveniet id harum illum corrupti
-            aut eligendi blanditiis porro! Repudiandae maiores, unde similique
-            cupiditate cum error!
+          <p className="abt_social">
+            Currently I am working as a FullStack Developer Intern at
+            MindWebsVentures. If you want to collaborate with me for a project,
+            do reach out to me through my socials:{" "}
+            <div className="abt_socials">
+              <span>
+                <a
+                  href="https://github.com/SayanMunshi-blank36"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FiGithub className="social_icons" />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://twitter.com/blank_36"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FiTwitter className="social_icons" />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://www.linkedin.com/in/sayan-munshi-714a401b8/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FiLinkedin className="social_icons" />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://www.instagram.com/blank_36_r/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FiInstagram className="social_icons" />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="https://sayanmunshi-blank36.hashnode.dev/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <SiHashnode className="social_icons" />
+                </a>
+              </span>
+              <span>
+                <a
+                  href="mailto:sayanmunshi36@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FiMail className="social_icons" />
+                </a>
+              </span>
+            </div>
           </p>
           <p>Here are a few technologies I’ve been exploring recently:</p>
           <div className="tech_sec">
@@ -85,6 +150,10 @@ const About = () => {
               <li>
                 <SiWeb3Dotjs className="tech_icons" />
                 Web 3.0
+              </li>
+              <li>
+                <SiFigma className="tech_icons" />
+                Figma
               </li>
             </ul>
           </div>
