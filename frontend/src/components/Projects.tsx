d7d6ab5622a1bc334a675499d4c3410b5ed4d2a3
Sayan <sayanmunshi36@gmail.com>
2023-02-06 00:13:13 +0530

    Demo Project Section

--- frontend/src/components/Projects.tsx
+++ frontend/src/components/Projects.tsx
@@ -4,6 +4,13 @@ const Projects = () => {
   return (
     <section className="project_section">
       <h1 className="pro_head">Projects.</h1>
+      <div className="project_content">
+        <div className="project_box"></div>
+        <div className="project_box"></div>
+        <div className="project_box"></div>
+        <div className="project_box"></div>
+        <div className="project_box"></div>
+      </div>
     </section>
   );
 };
